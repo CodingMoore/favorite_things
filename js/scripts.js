577541beb9789a2b2493f70c2245e792c74a5b3e
Randel M <Randel.C.Moore@gmail.com>
2020-10-26 16:12:45 -0700

    add code to allow swaping of user inputs between arrays

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,29 @@
-// $(document).ready(function() {
-//   $(document).submit(function(event) {
-//     const inputVal1 = $("#faveInput1").val();
-//     const inputVal2 = $("#faveInput2").val();
-//     const inputVal3 = $("#faveInput3").val();
-//     let inputFinal = [inputVal1, inputVal2, inputVal3];
-//     alert(inputFinal);
-//     event.preventDefault();
-//   });
-// });
-
-//Please undo this stuff if it is not making sense. What really helps me is to go with that works/makes sense then refactor (clean up) later. - Cathy 
-//You're all doing awesome!
-
-/*function listOfThings(inputVal){
-  let thingsList = [];
-  thingsList.push(inputVal);
-  favThings(thingsList);
-  return thingsList; 
-}
-
-function favThings(thingsList) {
-  let newThingsList = thingsList.join(", ");
-  return newThingsList;
-}*/
 
 $(document).ready(function() {
   let thingsList = []
+  let superThingsList = []
   $(document).submit(function(event) {
     const inputVal = $("#faveInput").val();
     thingsList.push(inputVal)
     $("#thingsOutput").text(thingsList);
+    //$("#superThingsOutput").text(superThingsList);
     event.preventDefault();  
   });
-    $("#super").click(function() {   
-      thingsList.pop();
-      alert(thingsList);  
-    });
+  $("#super").click(function() {   
+    const spliceVal1 = thingsList.splice(0,1);
+    superThingsList.push(spliceVal1);
+    $("#thingsOutput").text(thingsList);
+    $("#superThingsOutput").text(superThingsList);
+    alert(thingsList);
+    alert(superThingsList);
+  });
+  $("#favorite").click(function() {   
+    const spliceVal2 = superThingsList.splice(0,1);
+    thingsList.push(spliceVal2);
+    $("#thingsOutput").text(thingsList);
+    $("#superThingsOutput").text(superThingsList);
+    alert(thingsList);
+    alert(superThingsList);
+  });
+
 });
